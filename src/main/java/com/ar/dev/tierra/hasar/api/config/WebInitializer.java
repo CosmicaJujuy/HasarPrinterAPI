ac9455e9ee940da1243229fd171d322479989cde
Paulo Galdo <paulo.galdo@softlogia.com>
2017-10-22 00:34:49 -0300

    Actualizacion de librerias

--- src/main/java/com/ar/dev/tierra/hasar/api/config/WebInitializer.java
+++ src/main/java/com/ar/dev/tierra/hasar/api/config/WebInitializer.java
@@ -2,7 +2,7 @@ package com.ar.dev.tierra.hasar.api.config;
 
 import com.ar.dev.tierra.hasar.api.HasarPrinterAPI;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  *
